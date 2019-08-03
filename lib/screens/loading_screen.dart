4eff011d7c356bbd72b4f278b7622f4ab9463883
Olex Filipenko <olex.filipenko.stratis@gmail.com>
2019-08-03 13:39:26 +0300

    [FUTURE] - Get weather

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import '../services/location.dart';
+import '../services/networking.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+const String apiKey = '480f6778cd4f695dbed6c9c2db72c0fd';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,16 +12,32 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  var data;
+
+  @override
+  void initState() {
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getLocation();
+
+    String dataUrl =
+        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey';
+    NetworkHelper networkHelper = NetworkHelper(dataUrl);
+    data = await networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
+        child: SpinKitWave(color: Colors.white, size: 100.0),
       ),
     );
   }
---
+++ lib/services/networking.dart
@@ -0,0 +1,23 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class NetworkHelper {
+  final String url;
+
+  NetworkHelper(this.url);
+
+  Future<dynamic> getData() async {
+    double temperature;
+    int id;
+    String cityName;
+
+    http.Response response = await http.get(this.url);
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      return data;
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
